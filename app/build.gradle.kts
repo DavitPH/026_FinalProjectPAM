37bfd05b3033ee1c2074526893bb5d732bb6b034
DavitPH <108072024+DavitPH@users.noreply.github.com>
2024-01-07 14:03:54 +0700

    mengganti compileSdk 33 menjadi 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.a026_finalprojectpam"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.a026_finalprojectpam"
