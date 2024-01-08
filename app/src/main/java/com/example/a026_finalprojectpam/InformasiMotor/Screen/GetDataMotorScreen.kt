1b90f1709c8dba1bf7077367f420f5be77ae0a0e
DavitPH <108072024+DavitPH@users.noreply.github.com>
2024-01-09 01:25:41 +0700

    menambahkan var di get data motor screen

--- app/src/main/java/com/example/a026_finalprojectpam/InformasiMotor/Screen/GetDataMotorScreen.kt
+++ app/src/main/java/com/example/a026_finalprojectpam/InformasiMotor/Screen/GetDataMotorScreen.kt
@@ -1,6 +1,11 @@
 package com.example.a026_finalprojectpam.InformasiMotor.Screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import com.example.a026_finalprojectpam.InformasiMotor.MotorViewModel
 
@@ -9,5 +14,14 @@ fun GetDataMotorScreen(
     navController: NavController,
     motorViewModel: MotorViewModel,
 ){
+    var idMotor: String by remember { mutableStateOf("") }
+    var nmMotor: String by remember { mutableStateOf("") }
+    var pltMotor: String by remember { mutableStateOf("") }
+    var wrnMotor: String by remember { mutableStateOf("") }
+    var hrgMotor: String by remember { mutableStateOf("") }
+
+    val context = LocalContext.current
+
+
 
 }
