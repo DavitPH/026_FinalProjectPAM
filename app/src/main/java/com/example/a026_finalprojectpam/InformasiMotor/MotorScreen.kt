ddc16de82aa6d15203c32cca661ed787d364aa5c
DavitPH <108072024+DavitPH@users.noreply.github.com>
2024-01-09 00:37:59 +0700

    menambahkan Motor Screen

---
+++ app/src/main/java/com/example/a026_finalprojectpam/InformasiMotor/MotorScreen.kt
@@ -0,0 +1,12 @@
+package com.example.a026_finalprojectpam.InformasiMotor
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun MotorScreen(
+    navController: NavController,
+    motorViewModel: MotorViewModel,
+){
+
+}
