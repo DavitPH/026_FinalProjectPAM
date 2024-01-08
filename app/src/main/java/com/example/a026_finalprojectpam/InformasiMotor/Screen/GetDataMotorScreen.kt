dd3425606cf1b4b7db5579e314c3eafcbec3480d
DavitPH <108072024+DavitPH@users.noreply.github.com>
2024-01-09 00:40:35 +0700

    menambahkan Get Data Motor Screen

---
+++ app/src/main/java/com/example/a026_finalprojectpam/InformasiMotor/Screen/GetDataMotorScreen.kt
@@ -0,0 +1,13 @@
+package com.example.a026_finalprojectpam.InformasiMotor.Screen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.a026_finalprojectpam.InformasiMotor.MotorViewModel
+
+@Composable
+fun GetDataMotorScreen(
+    navController: NavController,
+    motorViewModel: MotorViewModel,
+){
+
+}
--- app/src/main/java/com/example/a026_finalprojectpam/InformasiMotor/Screen/MotorScreen.kt
+++ app/src/main/java/com/example/a026_finalprojectpam/InformasiMotor/Screen/MotorScreen.kt
@@ -1,7 +1,8 @@
-package com.example.a026_finalprojectpam.InformasiMotor
+package com.example.a026_finalprojectpam.InformasiMotor.Screen
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import com.example.a026_finalprojectpam.InformasiMotor.MotorViewModel
 
 @Composable
 fun MotorScreen(
