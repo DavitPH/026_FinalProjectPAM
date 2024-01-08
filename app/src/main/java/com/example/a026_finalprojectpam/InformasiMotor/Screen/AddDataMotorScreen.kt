3422d4931972b6616d697506c24497e4205c8496
DavitPH <108072024+DavitPH@users.noreply.github.com>
2024-01-09 01:13:23 +0700

    membuat navigasi halaman dan update AddDataMotorScreen

--- app/src/main/java/com/example/a026_finalprojectpam/InformasiMotor/Screen/AddDataMotorScreen.kt
+++ app/src/main/java/com/example/a026_finalprojectpam/InformasiMotor/Screen/AddDataMotorScreen.kt
@@ -1,2 +1,13 @@
 package com.example.a026_finalprojectpam.InformasiMotor.Screen
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.a026_finalprojectpam.InformasiMotor.MotorViewModel
+
+@Composable
+fun AddDataMotorScreen (
+    navController: NavController,
+    motorViewModel: MotorViewModel,
+){
+
+}
--- app/src/main/java/com/example/a026_finalprojectpam/Navigasi/NavigasiHalaman.kt
+++ app/src/main/java/com/example/a026_finalprojectpam/Navigasi/NavigasiHalaman.kt
@@ -1,1 +1,41 @@
 package com.example.a026_finalprojectpam.Navigasi
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.a026_finalprojectpam.InformasiMotor.MotorViewModel
+import com.example.a026_finalprojectpam.InformasiMotor.Screen.GetDataMotorScreen
+import com.example.a026_finalprojectpam.InformasiMotor.Screen.AddDataMotorScreen
+import com.example.a026_finalprojectpam.InformasiMotor.Screen.MotorScreen
+
+@Composable
+fun NavigasiHalaman(
+    navController: NavHostController,
+    motorViewModel: MotorViewModel
+){
+    NavHost(
+        navController = navController,
+        startDestination = Screens.MotorScreen.route
+    ) {
+        // main screen
+        composable(
+            route = Screens.MotorScreen.route
+        ) {
+            MotorScreen(navController = navController, motorViewModel = motorViewModel)
+        }
+        // get data screen
+        composable(
+            route = Screens.GetDataMotorScreen.route
+        ) {
+            GetDataMotorScreen(navController = navController, motorViewModel = motorViewModel)
+
+        }
+        // add data screen
+        composable(
+            route = Screens.AddDataMotorScreen.route
+        ){
+            AddDataMotorScreen(navController = navController, motorViewModel = motorViewModel)
+        }
+    }
+}
