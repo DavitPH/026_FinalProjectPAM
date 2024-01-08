c2433e96b5f71371cd8f3ef9d44e4d199442e782
DavitPH <108072024+DavitPH@users.noreply.github.com>
2024-01-09 00:29:12 +0700

    membuat function retrieve data

--- app/src/main/java/com/example/a026_finalprojectpam/InformasiMotor/MotorViewModel.kt
+++ app/src/main/java/com/example/a026_finalprojectpam/InformasiMotor/MotorViewModel.kt
@@ -5,6 +5,7 @@ import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObject
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -23,6 +24,30 @@ class MotorViewModel(): ViewModel(){
             fireStoreRef.set(dataMotor)
                 .addOnSuccessListener {
                     Toast.makeText(context, "Succsesfully saved data", Toast.LENGTH_LONG).show()
+                }
+        } catch (e: Exception){
+            Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun retrieveData(
+        idMotor: String,
+        context: Context,
+        data: (DataMotor) -> Unit
+    )    = CoroutineScope(Dispatchers.IO).launch {
+        val fireStoreRef = Firebase.firestore
+            .collection("motor")
+            .document(idMotor)
+
+        try{
+            fireStoreRef.get()
+                .addOnSuccessListener {
+                   if (it.exists()){
+                       val dataMotor = it.toObject<DataMotor>()!!
+                       data(dataMotor)
+                   } else {
+                       Toast.makeText(context, "No User Data Found", Toast.LENGTH_SHORT).show()
+                   }
                 }
         } catch (e: Exception){
             Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
